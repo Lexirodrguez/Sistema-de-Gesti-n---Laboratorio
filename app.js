26353ebe7e1b04df77643efd84f54aef73fd19e6
Lexirodrguez <rodriguezmatheuslexc@uvm.edu.ve>
2026-02-02 17:17:43 -0400

    Configuracion de app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pacientesRouter = require("./routes/pacientes");
+var examenesRouter = require("./routes/examenes");
+var resultadosRouter = require("./routes/resultados");
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/pacientes", pacientesRouter);
+app.use("/examenes", examenesRouter);
+app.use("/resultados", resultadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
