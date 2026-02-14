1c5cddba199eb4192b0d1e4bbb8ce6810fd30ff4
Lexirodrguez <rodriguezmatheuslexc@uvm.edu.ve>
2026-02-13 23:48:52 -0400

    examenes modelos con promesas

--- configuracion_bd/bd.js
+++ configuracion_bd/bd.js
@@ -1,13 +1,10 @@
-const mysql = require("mysql2/promise")
+const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "laboratorio",
-    aitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'laboratorio',
 });
 
-module.exports = pool;
+module.exports = connection;
--- modelos/examenes_modelos.js
+++ modelos/examenes_modelos.js
@@ -2,56 +2,52 @@ const db = require("../configuracion_bd/bd.js");
 
 class Modeloexamenes {
     
-    async todos () {
-        try {
-            const [completo] = await db.query("SELECT * FROM examenes");
-            return completo;  
-        } catch (error) {
-            return []
-        }
+    todos () {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM examenes", (error, resultado) => {
+                if (error) return reject(error);
+                resolve(resultado);
+            });
+        });
     }
 
-    async buscarporId(id) {
-        try {
-            const [encontrar] = await db.query("SELECT * FROM examenes WHERE id_examenes = ?", [id]);
-            return encontrar.length > 0 ? encontrar[0] : null;
-        } catch (error) {
-            return null;
-        }
+    buscarporId(id) {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM examenes WHERE id_examenes = ?", [id], (error, resultado) => {
+                if (error) return reject(error);
+                resolve(resultado[0] || null );
+            });
+        });    
     }
 
-    async crear(examenNuevo) {
-        try {
-            const  {nombre_examenes, precio_examenes, descripcion_examenes } = examenNuevo;
-            const [nuevo] = await db.query(
-                "INSERT INTO examenes (nombre_examenes, precio_examenes, descripcion_examenes) VALUES (?, ?, ?)",
-                [nombre_examenes, precio_examenes, descripcion_examenes]
-            );
-            return { id_examenes: nuevo.insertId, ...examenNuevo };
-        } catch (error) {
-            return null; 
-        }
+    crear(examenNuevo) {
+        return new Promise((resolve, reject) => {
+            const { nombre_examenes, precio_examenes, descripcion_examenes } = examenNuevo;
+            const sql = "INSERT INTO examenes (nombre_examenes, precio_examenes, descripcion_examenes) VALUES (?, ?, ?)";
+
+            db.query(sql, [nombre_examenes, precio_examenes, descripcion_examenes], (error, resultado) => {
+                if (error) return reject(error);
+                resolve({id_examenes: resultado.insertId, ...datos});
+            });
+        });
     }
 
-    async actualizar(id, examenActualizado) {
-        try {
-            const [nuevo] = await db.query(
-                "UPDATE examenes SET ? WHERE id_examenes = ?",
-                [examenActualizado, id]
-            );
-            return nuevo.affectedRows > 0 ? { id_examenes: id, ...examenActualizado } : null;
-            } catch (error) {
-                return null;
-            }
-        }
+    actualizar(id, examenActualizado) {
+        return new Promise((resolve, reject) => {
+            db.query("UPDATE examenes SET ? WHERE id_examenes = ?", [id, examenActualizado], (error, resultado) => {
+                if (error) return reject(error);
+                resolve(resultado.affectedRows > 0 ? {id_examenes: id, ...examenActualizado } : null);
+            });
+        });
+    }
 
-        async eliminar(id) {
-            try {
-                const [encontrar] = await db.query("DELETE FROM examenes WHERE id_examenes = ?", [id]);
-                return encontrar.affectedRows > 0;
-            } catch (error) {
-                return null;
-            }
+    eliminar(id) {
+           return new Promise((resolve, reject) => {
+               db.query("DELETE FROM examenes WHERE id_examenes = ?", [id], (error, resultado) => {
+                if (error) return reject(error);
+                resolve(resultado.affectedRows > 0);
+               });
+           });
         }
     }
 
